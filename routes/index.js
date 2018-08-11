6a73d4cf71f5981dc690ac1bcaddd610aa2001d2
aldo <aldouemoto@gmail.com>
2018-08-11 17:09:26 -0300

    Construir HTML e CSS

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Meu primeiro Webapp incrível!' });
 });
+
+
+/* GET test page. */
+router.get('/test', function(req, res, next) {
+  res.render('index', { title: 'Teste', status: 'Very Happy' });
+});
+
+
+router.get('/helicopterson', function(req, res, next) {
+  res.render('bio', { title: 'Teste', status: 'Very Happy' });
+});
+
+
 
 module.exports = router;
